69f41837fde7a088926b1432923aafd2ddf5d25b
jeff0s <culat@Vador.local>
2015-05-17 12:56:28 +0200

    update test invalid input for log append

--- tests/Test_InvalideInput_logappend.sh
+++ tests/Test_InvalideInput_logappend.sh
@@ -18,27 +18,41 @@ rm -f $HOME/.logdb
 ../build/logappend -T 4 -K secret -A -G Fre01d log1
 ../build/logappend -T 5 -K secret -A -G Fre@d log1
 
+#Incorrect Timestamp
+rm -f log1
+rm -f $HOME/.logdb
+../build/logappend -T 0 -K secret -A -G Fred log1
+../build/logappend -T 1 -K secret -A -G Jane log1
+../build/logappend -T 2 -K secret -A -G Bob log1
+echo "Error time stamp expected"
+../build/logappend -T 1 -K secret -A -G Alex log1
+
 
 
 #Incorrect Token
 rm -f log1
 rm -f $HOME/.logdb
-../build/logappend -T 0 -K secret -A -G 01Fred log1
-../build/logappend -T 1 -K secret -A -G Fred01 log1
-../build/logappend -T 2 -K secret -A -G @Fred log1
-../build/logappend -T 1 -K secret -A -G Fre01d log1
 
+../build/logappend -T 0 -K secret0 -A -G Fred log1
+echo "Invalid expected: Error on Token"
+../build/logappend -T 0 -K secret1 -A -G Jane log1
 
-
-#Incorrect secret
-
+rm -f log1
+rm -f $HOME/.logdb
+echo "Invalid expected: Error on Token"
+../build/logappend -T 0 -K secret@ -A -G Fred log1
 
 
 #Incorrect option -A / -L
+rm -f log1
+rm -f $HOME/.logdb
+echo "Invalid expected: Error on option"
+../build/logappend -T 0 -K secret0 -N -G Fred log1
 
 
+#Incorrect RoomID
 
-#Incorrect Timestamp
+
 
 
 
