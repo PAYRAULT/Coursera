e070512f7c90179a217b695b72978b1adc51f3b1
jeff0s <culat@Vador.local>
2015-05-31 11:20:04 +0200

    Update of FullTest.py

--- tests/FullTest.py
+++ tests/FullTest.py
@@ -3,7 +3,7 @@ import random
 import os
 
 ListeOfGuest=[]
-build=["../107/code/build/","../111/code/build/"] # A generer
+build=["../107/code/build/","../111/code/build/","../114/code/build/"] # A generer
 ListOfToken=range(1,1073741823,1000000)
 
 def allT():
@@ -16,7 +16,6 @@ def allT():
 			cmd=os.popen(log+"logappend"+" -T "+ str(i)+" -K "+"secret"+ " -A " + "-G "+guest+" log1")
 			cmd
 			result = cmd.read()
-			print "T = "+str(i)
 			if "invalid" in result:
 				print "Error with: "+"guest: "+ guest+ " Token: "+ str(i)
 
@@ -29,16 +28,18 @@ def allT():
 			print "Error with: "+"guest: "+ guest+ " Token: "+ str(i)
 
 
-		print "Result logread"
+		
 		cmd=os.popen(log+"logread"+" -K "+"secret"+ " -S "+"log1")
 		cmd
 		result=cmd.read()
-		print result
+		if "invalid" in result:
+			print "Error logread"
+		#print result
 		os.system("rm -f log1")
 
 def allsecret():
 	i=0
-	secret=createlstsecret(65536)
+	secret=createlstsecret(65535)
 	for log in build:
 		print "***********"+log+"***********"
 		for s in secret:
@@ -49,15 +50,16 @@ def allsecret():
 			cmd2
 			result1=cmd1.read()
 			if "invalid" in result1:
-				print "Error with: "+"Key: "+ s+ "in log"+ str(i)
+				print "Logappend Error with: "+"Key: "+ s+ "in log"+ str(i)
 			result2=cmd2.read()
-			print result2
-			
+			if "invalid" in result2:
+				print "Logread Error with: "+"Key: "+ s+ "in log"+ str(i)
+		os.system("rm -f log*")		
 
 
 def createlstsecret(size):
 	s=[]
-	for i in range(1000000):
+	for i in range(100):
 		chars=string.ascii_uppercase+string.ascii_lowercase+string.digits
 		g = ''.join(random.choice(chars) for _ in range(size))
 		while(g in s):
@@ -77,5 +79,5 @@ def chercherguest():
 	return g
 
 
-#allT()
+allT()
 allsecret()
